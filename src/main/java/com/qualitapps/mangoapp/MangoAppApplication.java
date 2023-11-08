da399cf8bb9c7a01aad3a0ea3c76eee2c5cca520
minoliAlles <141716046+minoliAlles@users.noreply.github.com>
2023-11-08 11:32:24 +0530

    initial commit

---
+++ src/main/java/com/qualitapps/mangoapp/MangoAppApplication.java
@@ -0,0 +1,13 @@
+package com.qualitapps.mangoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qualitapps/mangoapp/MangoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualitapps.mangoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
